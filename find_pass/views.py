99e1aadade53b20e23057272678d42e2523cabe7
Levin <dushiyi319@163.com>
2018-03-23 17:31:34 +0800

    tmp

--- find_pass/views.py
+++ find_pass/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+class User(object):
+	"""docstring for User"""
+	def __init__(self, arg):
+		super(User, self).__init__()
+		self.arg = arg
+		
